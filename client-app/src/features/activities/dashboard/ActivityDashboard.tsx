d1ae83ad5c1e6d002dd5f52efd104b7f618fd656
Aziz, Chiara <chiara_aziz@optum.com>
2023-02-24 14:03:27 -0500

    delete buton added and funtionality working

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,5 @@
-import React, { Fragment } from "react";
-import { Grid, List } from "semantic-ui-react";
+import React from "react";
+import { Grid} from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
@@ -14,14 +14,15 @@ interface Props {
     openForm: (id:string) => void;
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
 }
 
 export default function ActivityDashboard({activities, selectedActivity, selectActivity, cancelSelectActivity,
-                                            editMode, openForm, closeForm, createOrEdit}: Props) {
+                                            editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} selectActivity={selectActivity}></ActivityList>
+                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity}></ActivityList>
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
