c252e4c3a14a5952516142855914b17530c6f846
Aziz, Chiara <chiara_aziz@optum.com>
2023-03-27 16:26:51 -0500

    toast has an error with axios and isn't working - just console logs

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -21,7 +21,8 @@ namespace API.Controllers
         [HttpGet("{id}")] //api/activities/kfjsdkfjsldf
         public async Task<IActionResult> GetActivity(Guid id)
         {
-            return HandleResult(await Mediator.Send(new Details.Query{Id = id}));
+           
+            return HandleResult(await Mediator.Send(new Details.Query{Id = id}));      
                 
         }
 
--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -14,9 +14,9 @@ export default function NavBar () {
                     <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
                     Reactivities
                 </Menu.Item>
-                <Menu.Item as={NavLink} to='/activities' name="Activities">
+                <Menu.Item as={NavLink} to='/activities' name="Activities" />
+                <Menu.Item as={NavLink} to='/errors' name="Errors" />
 
-                </Menu.Item>
                 <Menu.Item>
                     <Button as={NavLink} to='/createActivity' positive content='Create Activity'/>
                 </Menu.Item>
