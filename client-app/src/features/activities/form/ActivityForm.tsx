e5ce474cd7c7e1171a4a855dc35ca21505245278
Aziz, Chiara <chiara_aziz@optum.com>
2023-04-05 16:10:16 -0500

    edited the date a bit further

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,7 +1,7 @@
 import { observer } from "mobx-react-lite";
 import { ChangeEvent, useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { Button, Label, Segment } from "semantic-ui-react";
+import { Button, Header, Label, Segment } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { Activity } from "../../../app/models/activity";
 import { useStore } from "../../../app/stores/store";
@@ -29,7 +29,7 @@ export default observer (function ActivityForm() {
         title: '',
         category: '',
         description: '',
-        date: '',
+        date: null,
         city: '',
         venue: ''
     });
@@ -38,7 +38,7 @@ export default observer (function ActivityForm() {
         title: Yup.string().required('The activity title is required.'),
         description: Yup.string().required('The activity description is required.'),
         category: Yup.string().required(),
-        date: Yup.string().required(),
+        date: Yup.string().required('Date is required.'),
         venue: Yup.string().required(),
         city: Yup.string().required()
     });
@@ -48,32 +48,28 @@ export default observer (function ActivityForm() {
     }, [id, loadActivity]);
     
 
-    // function handleSubmit() {
-    //    if (!activity.id) {
-    //        activity.id = uuid();
-    //        createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
-    //    } else {
-    //        updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+    function handleFormSubmit(activity: Activity) {
+       if (!activity.id) {
+           activity.id = uuid();
+           createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+       } else {
+           updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
 
-    //    }
-    // }
+       }
+    }
 
-    // function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-    //     const {name, value} = event.target;
-    //     setActivity({...activity, [name]: value})
-
-    // }
 
     if (loadingInitial) return <LoadingComponent content="Loading Activity ..."/>;
 
     return (
         <Segment clearing>
+            <Header content='Activity Details' sub color='teal'/>
             <Formik 
                 validationSchema={validationSchema}
                 enableReinitialize 
                 initialValues={activity} 
-                onSubmit={(values) => console.log(values)}>
-                {({ handleSubmit}) => (
+                onSubmit={(values) => handleFormSubmit(values)}>
+                {({ handleSubmit, isValid, isSubmitting, dirty}) => (
                     <Form className="ui form" onSubmit={handleSubmit} autoComplete='off'>
                         <MyTextInputs placeholder={"Title"} name={"title"}/>
                         <MyTextArea  placeholder='Description' name='description'  />
@@ -84,9 +80,12 @@ export default observer (function ActivityForm() {
                             showTimeSelect
                             timeCaption="time"
                             dateFormat={'MMMM d, yyyy h:m aa'}/>
+                        <Header content='Location Details' sub color='teal'/>
                         <MyTextInputs placeholder='City'  name='city' />
                         <MyTextInputs placeholder='Venue'  name='venue' />
-                        <Button loading={loading} floated="right" positive type="submit" content='Submit'/>
+                        <Button 
+                            disabled={isSubmitting || !dirty || !isValid}
+                            loading={loading} floated="right" positive type="submit" content='Submit'/>
                         <Button as={Link} to='/activities' floated="right"  type="submit" content='Cancel'/>
                     </Form>
                 )}
@@ -96,3 +95,7 @@ export default observer (function ActivityForm() {
         </Segment>
     )
 })
+
+function uuid(): string {
+    throw new Error("Function not implemented.");
+}
