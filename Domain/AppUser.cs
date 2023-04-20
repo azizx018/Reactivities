749d1afed6af92850706941a3c2ace4d4b196959
Aziz, Chiara <chiara_aziz@optum.com>
2023-04-20 16:41:20 -0500

    adding photo table

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -4,8 +4,11 @@ namespace Domain
 {
     public class AppUser : IdentityUser
     {
+    
+
         public string DisplayName {get; set;}
         public string Bio {get; set;}
-         public ICollection<ActivityAttendee> Activities {get; set;}
+        public ICollection<ActivityAttendee> Activities {get; set;}
+        public ICollection<Photo> Photos {get; set;}
     }
 }
