b84a8c48dd22329fc3afb21d3e39149d0f5b28c3
Aziz, Chiara <chiara_aziz@optum.com>
2023-04-24 16:48:41 -0500

    creating profile store

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -5,6 +5,7 @@ import { router } from "../Router/Routes";
 import { store } from "../stores/store";
 import { User, UserFormValues } from "../models/user";
 import { config } from "process";
+import { Profile } from "../models/profile";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -84,9 +85,14 @@ const Account = {
     register: (user:UserFormValues) => requests.post<User>('/account/register', user)
 }
 
+const Profiles = {
+    get: (username: string) => requests.get<Profile>(`/profiles/${username}`)
+}
+
 const agent = {
     Activities,
-    Account
+    Account,
+    Profiles
 }
 
 export default agent;
--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,5 +1,15 @@
+import { Grid } from "semantic-ui-react";
+import ProfileContent from "./ProfileContent";
+import ProfileHeader from "./ProfileHeader";
+
 export default function ProfilePage() {
     return(
-        <h1>Profile</h1>
+        <Grid>
+            <Grid.Column width={16}>
+                <ProfileHeader/> 
+                <ProfileContent/>
+            </Grid.Column>
+
+        </Grid>
     )
 }
